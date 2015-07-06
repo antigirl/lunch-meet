e5814a545005309c405f2bf65a6a1f90ebebcb55
Farzad Qasim <qasimf@tcd.ie>
2015-07-06 17:38:44 +0100

    API does not dispatch events, it just returns a promise and the actions then resolve with dispatched events

--- app/js/actions/appActions.js
+++ app/js/actions/appActions.js
@@ -1,5 +1,6 @@
 var appDispatcher = require('../dispatcher/appDispatcher');
 var fourSquareApi = require('../utils/fourSquareApi');
+var Promise = require('Promise');
 
 module.exports = {
     connectFourSquare: function() {
@@ -8,13 +9,31 @@ module.exports = {
         });
     },
     isConnected: function() {
-        fourSquareApi.isConnected();
+        fourSquareApi.isConnected().then(function() {
+            appDispatcher.dispatch({
+                type: 'isConnected'
+            });
+        });
     },
-
     getCurrentLocation: function() {
-        fourSquareApi.getCurrentLocation();
+        fourSquareApi.getCurrentLocation().then(function(location) {
+            appDispatcher.dispatch({
+                type: 'location',
+                response: location
+            });
+        }).catch(function() {
+            appDispatcher.dispatch({
+                type: 'error',
+                response: errors
+            });
+        });
     },
     venueSearch: function() {
-        fourSquareApi.venueSearch();
+        fourSquareApi.venueSearch().then(function(body) {
+            appDispatcher.dispatch({
+                type: 'venueSearch',
+                response: JSON.parse(body)
+            });
+        });
     }
 };
--- app/js/utils/fourSquareApi.js
+++ app/js/utils/fourSquareApi.js
@@ -1,6 +1,7 @@
 var appDispatcher = require('../dispatcher/appDispatcher');
 var request = require('browser-request');
 var stub = require('./stub.json');
+var Promise = require('Promise');
 
 module.exports = {
 
@@ -13,54 +14,49 @@ module.exports = {
 
     isConnected: function() {
         var accessToken = window.location.hash;
-        if(accessToken) {
+        if (accessToken) {
             this.accessToken = accessToken.replace('#access_token=', '');
-            appDispatcher.dispatch({
-                type: 'isConnected'
-            });
+            return Promise.resolve();
         }
+
+        return Promise.reject();
     },
     getCurrentLocation: function() {
         var _this = this;
-        navigator.geolocation.getCurrentPosition(function(position){
+        return new Promise(function(resolve, reject) {
+            navigator.geolocation.getCurrentPosition(function(position) {
                 var location = {
-                 lat: position.coords.latitude,
-                 long: position.coords.longitude
-             };
+                    lat: position.coords.latitude,
+                    long: position.coords.longitude
+                };
 
-             _this.location = location;
+                _this.location = location;
+                resolve(location);
 
-              appDispatcher.dispatch({
-                  type: 'location',
-                  response: location
-              });
-
-        }, function(error) {
+            }, function(error) {
                 var errors = 'error:' + error.message;
+                reject(errors);
 
-                appDispatcher.dispatch({
-                    type: 'error',
-                    response: errors
-                });
-
-        }, {timeout:10000});
+            }, {
+                timeout: 10000
+            });
+        });
     },
     venueSearch: function() {
-        var venueSearch = 'https://api.foursquare.com/v2/venues/explore?ll='+this.location.lat+','+this.location.long+'&oauth_token='+this.accessToken+'&v=20150628&section=food&venuePhotos=1&limit=10';
-        request(venueSearch, function(err, response, body) {
-            if(!err) {
-              appDispatcher.dispatch({
-                  type: 'venueSearch',
-                  response: JSON.parse(body)
-              });
-            }
+        var venueSearch = 'https://api.foursquare.com/v2/venues/explore?ll=' + this.location.lat + ',' + this.location.long + '&oauth_token=' + this.accessToken + '&v=20150628&section=food&venuePhotos=1&limit=10';
+        return new Promise(function(resolve, reject) {
+            request(venueSearch, function(err, response, body) {
+                if (!err) {
+                    resolve(body);
+                }
+            });
         });
 
-         //cb(stub);
+        //cb(stub);
     }
 };
 
 var CLIENT_ID = 'PANFWAX4IHWMHJSZ3NUOTGFJL523INIJWPIPLCFO0TL1R4HI';
 var CLIENT_SECRET = 'RFPPUYUAJUVJWL5WNVZMQB2CPJ4FUFI2N1B4CLTBHYVOB0O3';
 var REDIRECT_URI = 'http://localhost:8080';
-var GET_TOKEN_URL = 'https://foursquare.com/oauth2/authenticate?client_id='+CLIENT_ID+'&response_type=token&redirect_uri='+REDIRECT_URI;
+var GET_TOKEN_URL = 'https://foursquare.com/oauth2/authenticate?client_id=' + CLIENT_ID + '&response_type=token&redirect_uri=' + REDIRECT_URI;
